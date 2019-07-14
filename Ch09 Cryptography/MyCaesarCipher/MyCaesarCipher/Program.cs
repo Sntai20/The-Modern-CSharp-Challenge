b58ff34ce0978ac6031a784120f680127b208973
Antonio Santana <antonio.santana.ir@outlook.com>
2019-07-14 12:47:24 -0400

    Completed encrypt method.

---
+++ Ch09 Cryptography/MyCaesarCipher/MyCaesarCipher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyCaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and intialize Plain Text, Key, and Cipher Text
+            string PlainText = "CMU";
+            int Key = 3;
+            string CipherText = "";
+
+            // Print the PT to the console.
+            Console.WriteLine($"Message in PlainText: {PlainText}");
+
+            // To encrypt PT message, use the Encrypt method w/ PT and Key.
+            CipherText = Utility.Encrypt(PlainText, Key);
+
+            // Print the CT to the console.
+            Console.WriteLine($"Message in CipherText: {CipherText}");
+            
+        }
+    }
+}
---
+++ Ch09 Cryptography/MyCaesarCipher/MyCaesarCipher/Utility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCaesarCipher
+{
+    internal class Utility
+    {
+        // Encryption Method Caesar
+        internal static string Encrypt(string PlainText, int Key)
+        {
+            // To shift characters, change strings to an array of characters.
+            // Select an appropriate data structure: array, circular linked list, 
+            //List<char> CipherText = new List<char>();
+            char[] chars = new char[PlainText.Length];
+
+            // Iterate through each char.
+            for (int i = 0; i < PlainText.Length; i++)
+            {
+                // Get the numerical value of each char.
+                int ch = PlainText[i] - 'A';
+                // To shift each char add the Key
+                ch = (ch + Key + 26) % 26;
+                // Cast the numerical value to char.
+                chars[i] = (char)('A' + ch);
+            }
+
+            // Cipher text "FPX"
+            string CipherText = new string(chars);
+            //Console.WriteLine(CipherText);
+            return CipherText;
+        }
+
+        // Decryption Method
+        //public static string Decrypt(string PlainText, int Key)
+        //{
+            
+        //}
+
+        
+
+
+
+
+
+
+
+        
+    }
+}
